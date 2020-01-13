450f14e9d9442cf772061759a41650254e51a77d
Brice VIGNAL <vignal.brice@gmail.com>
2020-01-13 23:18:18 +0100

    Dashboard Links

---
+++ js/dashboard.js
@@ -0,0 +1,14 @@
+{
+    'use-strict';
+    /** Dashboard Function */
+    $(async function () {
+
+        if(document.location.pathname.indexOf('../vues/dashboard.html')){
+            nav = $('li.nav-item');
+            chatNav = $('#chatnav')
+            chatNav.css('.active')
+        }
+
+    })
+
+}
